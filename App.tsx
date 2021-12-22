7e8db08d4e9380785ec6c0e27462d774fa48c6cf
elliehachemjob <elliehachemjob@gmail.com>
2021-12-22 12:05:52 +0200

    1st

---
+++ App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
+
+
+const App = () => {
+  return (
+    <View>
+      <Text>Hi</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default App;
